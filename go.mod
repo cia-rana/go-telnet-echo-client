272b01a90e37b74f90356d763a701b5d31ace669
cia-rana <kiwamura0314@gmail.com>
2018-12-12 21:12:54 +0900

    Init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/cia-rana/go-simple-telnet-client
+
+require (
+	github.com/mattn/go-runewidth v0.0.3 // indirect
+	github.com/nsf/termbox-go v0.0.0-20181027232701-60ab7e3d12ed
+	github.com/reiver/go-oi v0.0.0-20160325061615-431c83978379
+	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e
+)
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	oi "github.com/reiver/go-oi"
+	telnet "github.com/reiver/go-telnet"
+
+	termbox "github.com/nsf/termbox-go"
+)
+
+func main() {
+	host := flag.String("h", "localhost", "host")
+	port := flag.String("p", "23", "port")
+	flag.Parse()
+
+	// Init termbox to use the getchar
+	if err := termbox.Init(); err != nil {
+		panic(err)
+	}
+	termbox.SetCursor(0, 0)
+	termbox.HideCursor()
+
+	if err := telnet.DialToAndCall(*host+":"+*port, caller{}); err != nil {
+		fmt.Println(err)
+	}
+}
+
+type caller struct{}
+
+func (c caller) CallTELNET(ctx telnet.Context, w telnet.Writer, r telnet.Reader) {
+	quit := make(chan struct{}, 1)
+	defer close(quit)
+
+	// Write to telnet server
+	readBlocker := make(chan struct{}, 1)
+	go func() {
+		for {
+			switch ev := termbox.PollEvent(); ev.Type {
+			case termbox.EventKey:
+				if ev.Key == termbox.KeyCtrlC {
+					quit <- struct{}{}
+					return
+				}
+				if isASCII(ev.Ch) {
+					fmt.Printf("%c", ev.Ch)
+					oi.LongWrite(w, []byte{byte(ev.Ch)})
+					readBlocker <- struct{}{}
+				}
+			}
+		}
+	}()
+
+	// Read from telnet server
+	go func() {
+		var buffer [1]byte
+		p := buffer[:]
+		for {
+			<-readBlocker
+			r.Read(p)
+			fmt.Printf("%c", p[0])
+		}
+	}()
+
+	<-quit
+}
+
+func isASCII(r rune) bool {
+	return r <= '~'
+}
